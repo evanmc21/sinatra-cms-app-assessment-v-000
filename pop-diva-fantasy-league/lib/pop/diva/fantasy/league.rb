215d78685aaad5fed47d0b90668fb658af60e2b5
Evan Candler <emcandler21@gmail.com>
2018-04-23 17:17:09 +0000

    automatically backed up by learn

---
+++ pop-diva-fantasy-league/lib/pop/diva/fantasy/league.rb
@@ -0,0 +1,11 @@
+require "pop/diva/fantasy/league/version"
+
+module Pop
+  module Diva
+    module Fantasy
+      module League
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ pop-diva-fantasy-league/lib/pop/diva/fantasy/league/version.rb
@@ -0,0 +1,9 @@
+module Pop
+  module Diva
+    module Fantasy
+      module League
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
